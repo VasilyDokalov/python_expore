739a46af7428193c88ecc8d466e451b31408c4d6
VasilyDokalov <dokalov@outlook.com>
2024-12-12 14:02:54 +0300

    secondary

--- lesson_1.py
+++
@@ -1,7 +0,0 @@
-num = 12345
-product = 1
-while num != 0:
-    last_digit = num % 10
-    product = product * last_digit
-    num = num // 10
-print(product)
--- lesson_2.py
+++
@@ -1,6 +0,0 @@
-n=int(input())
-x=0
-while n!=0:
-    x=n%10
-    print(x)
-    n=n//10
